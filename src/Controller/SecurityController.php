3777bc8ab69a3ebf3d4004ee917d55cab7d0216d
Hugo Guitton <hguitton@vanksen.com>
2020-06-27 13:25:09 +0200

    Test

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -64,6 +64,7 @@ class SecurityController extends AbstractController
      */
     public function login(AuthenticationUtils $authenticationUtils): Response
     {
+        dump(posix_getpwuid(posix_geteuid())['name']);
         dump(get_current_user()).die;
         // get the login error if there is one
         $error = $authenticationUtils->getLastAuthenticationError();
