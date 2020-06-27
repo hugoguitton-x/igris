39a98d9905ed6876f7985ff1f7436e3e6287ad9e
Julien Guitton <juldrixx@gmail.com>
2020-06-27 17:02:07 +0200

    [FEATURE] Cron

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,13 +4,11 @@
 echo "Docker container has been started"
 
 echo "Install dependencies"
-php composer install
+composer install
 
 echo "Setup cron jobs"
 # Setup a cron schedule
-echo "* * * * * echo \"Hello world\" >> /var/log/cron_hello.log 2>&1
-* * * * * /usr/local/bin/php /usr/src/app/bin/console app:refresh-manga >> /var/log/cron_refresh.log 2>&1
-* * * * * /usr/local/bin/php -v >> /var/log/cron_php.log 2>&1
+echo "* * * * * /usr/local/bin/php /usr/src/app/bin/console app:refresh-manga >> /var/log/cron_refresh.log 2>&1
 # This extra line makes it a valid cron" > scheduler.txt
 
 crontab scheduler.txt
