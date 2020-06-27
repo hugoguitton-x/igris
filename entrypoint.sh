a7b0b87c843fa90d5a051578f2362e648ac20f63
Julien Guitton <juldrixx@gmail.com>
2020-06-27 17:37:07 +0200

    [FEATURE] Cron

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,24 +1,6 @@
 #!/bin/bash
-
-# Start the run once job.
-echo "Docker container has been started"
-
-echo "Install dependencies"
-composer install
-
-echo "Install database"
-php /usr/src/app/bin/console doctrine:migrations:migrate -n
-
-echo "Setup cron jobs"
-# Setup a cron schedule
-echo "
-* * * * * /usr/local/bin/php /usr/src/app/bin/console app:refresh-manga >> /var/log/cron_refresh.log 2>&1
-# This extra line makes it a valid cron" > scheduler.txt
-
-echo "Add cron jobs"
-crontab scheduler.txt
-cron -f &
-
-# Important don't delete
-echo "Running fpm to handle connections from nginx"
-php-fpm &
+DIR=/docker-entrypoint.d
+echo "Entrypoint execution ..."
+if [[ -d "$DIR" ]]; then /bin/run-parts --verbose "$DIR"; fi
+echo "Starting CMD execution : $@ ..."
+exec "$@"
