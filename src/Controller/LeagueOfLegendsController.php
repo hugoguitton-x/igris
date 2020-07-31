b86e35c8415170683a29f2805a9b72f1fcd1e8e9
Hugo Guitton <hugoguitton@orange.fr>
2020-07-31 11:04:18 +0200

    Revert test

--- src/Controller/LeagueOfLegendsController.php
+++ src/Controller/LeagueOfLegendsController.php
@@ -17,21 +17,6 @@ class LeagueOfLegendsController extends AbstractController
      */
     public function index()
     {
-        $discord = new Discord([
-            'token' => '',
-        ]);
-        
-        $discord->on('ready', function ($discord) {
-            echo "Bot is ready!", PHP_EOL;
-        
-            // Listen for messages.
-            $discord->on('message', function ($message, $discord) {
-                echo "{$message->author->username}: {$message->content}",PHP_EOL;
-            });
-        });
-        
-        $discord->run();
-        die;
         $summonerName = 'MorikamiX2';
 
         $accountId = $this->getAccountId($summonerName);
