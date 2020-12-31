cb64f17acb22b94e89d61ad42036d2e082d0ead5
Hugo Guitton <hugoguitton@orange.fr>
2020-12-31 22:27:21 +0100

    Update

--- entrypoint.sh
+++ entrypoint.sh
@@ -12,8 +12,8 @@ php /usr/src/app/bin/console doctrine:migrations:migrate -n
 echo "Setup cron jobs"
 # Setup a cron schedule
 echo "
-*/10 * * * * /usr/local/bin/php /usr/src/app/bin/console app:refresh-infos-manga >> /var/log/cron_refresh_infos_manga.log 2>&1
-0 18 * * * /usr/local/bin/php /usr/src/app/bin/console app:refresh-image-manga >> /var/log/cron_refresh_image_manga.log 2>&1
+0 */2 * * * /usr/local/bin/php /usr/src/app/bin/console app:refresh-infos-manga >> /var/log/cron_refresh_infos_manga.log 2>&1
+# 0 18 * * * /usr/local/bin/php /usr/src/app/bin/console app:refresh-image-manga >> /var/log/cron_refresh_image_manga.log 2>&1
 # This extra line makes it a valid cron" > scheduler.txt
 
 echo "Add cron jobs"
