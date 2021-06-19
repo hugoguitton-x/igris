cceb54944cd363322dec1b02b621332ac53940b8
Hugo Guitton <hugoguitton@orange.fr>
2021-06-19 12:14:30 +0200

    Fix

--- src/EventSubscriber/RedirectToPreferredLocaleSubscriber.php
+++ src/EventSubscriber/RedirectToPreferredLocaleSubscriber.php
@@ -28,62 +28,63 @@ use function Symfony\Component\String\u;
  */
 class RedirectToPreferredLocaleSubscriber implements EventSubscriberInterface
 {
-  private $urlGenerator;
-  private $locales;
-  private $defaultLocale;
+    private $urlGenerator;
+    private $locales;
+    private $defaultLocale;
 
-  public function __construct(UrlGeneratorInterface $urlGenerator, string $locales, string $defaultLocale = null)
-  {
+    public function __construct(UrlGeneratorInterface $urlGenerator, string $locales, string $defaultLocale = null)
+    {
 
-    $this->urlGenerator = $urlGenerator;
+        $this->urlGenerator = $urlGenerator;
 
-    $this->locales = explode('|', trim($locales));
-    if (empty($this->locales)) {
-      throw new \UnexpectedValueException('The list of supported locales must not be empty.');
+        $this->locales = explode('|', trim($locales));
+        if (empty($this->locales)) {
+            throw new \UnexpectedValueException('The list of supported locales must not be empty.');
+        }
+
+        $this->defaultLocale = $defaultLocale ?: $this->locales[0];
+
+        if (!\in_array($this->defaultLocale, $this->locales, true)) {
+            throw new \UnexpectedValueException(sprintf('The default locale ("%s") must be one of "%s".', $this->defaultLocale, $locales));
+        }
+
+        // Add the default locale at the first position of the array,
+        // because Symfony\HttpFoundation\Request::getPreferredLanguage
+        // returns the first element when no an appropriate language is found
+        array_unshift($this->locales, $this->defaultLocale);
+        $this->locales = array_unique($this->locales);
     }
 
-    $this->defaultLocale = $defaultLocale ?: $this->locales[0];
-
-    if (!\in_array($this->defaultLocale, $this->locales, true)) {
-      throw new \UnexpectedValueException(sprintf('The default locale ("%s") must be one of "%s".', $this->defaultLocale, $locales));
+    public static function getSubscribedEvents(): array
+    {
+        return [
+            KernelEvents::REQUEST => 'onKernelRequest',
+        ];
     }
 
-    // Add the default locale at the first position of the array,
-    // because Symfony\HttpFoundation\Request::getPreferredLanguage
-    // returns the first element when no an appropriate language is found
-    array_unshift($this->locales, $this->defaultLocale);
-    $this->locales = array_unique($this->locales);
-  }
+    public function onKernelRequest(RequestEvent $event): void
+    {
+        $request = $event->getRequest();
 
-  public static function getSubscribedEvents(): array
-  {
-    return [
-      KernelEvents::REQUEST => 'onKernelRequest',
-    ];
-  }
+        // Ignore sub-requests and all URLs but the homepage
+        if (!$event->isMasterRequest() || '/' !== $request->getPathInfo()) {
+            return;
+        }
+        // Ignore requests from referrers with the same HTTP host in order to prevent
+        // changing language for users who possibly already selected it for this application.
+        $referrer = $request->headers->get('referer');
+        if (null !== $referrer && u($referrer)->ignoreCase()->startsWith($request->getSchemeAndHttpHost())) {
+            return;
+        }
+        $preferredLanguage = $request->getPreferredLanguage($this->locales);
 
-  public function onKernelRequest(RequestEvent $event): void
-  {
-    $request = $event->getRequest();
-
-    // Ignore sub-requests and all URLs but the homepage
-    if (!$event->isMasterRequest() || '/' !== $request->getPathInfo()) {
-      return;
+        if ($preferredLanguage !== $this->defaultLocale) {
+            $response = new RedirectResponse($this->urlGenerator->generate('home_page', ['_locale' => $preferredLanguage]));
+            $event->setResponse($response);
+        } 
+        // else {
+        //     $response = new RedirectResponse($this->urlGenerator->generate('home_page', ['_locale' => $this->defaultLocale]));
+        //     $event->setResponse($response);
+        // }
     }
-    // Ignore requests from referrers with the same HTTP host in order to prevent
-    // changing language for users who possibly already selected it for this application.
-    $referrer = $request->headers->get('referer');
-    if (null !== $referrer && u($referrer)->ignoreCase()->startsWith($request->getSchemeAndHttpHost())) {
-      return;
-    }
-    $preferredLanguage = $request->getPreferredLanguage($this->locales);
-
-    if ($preferredLanguage !== $this->defaultLocale) {
-      $response = new RedirectResponse($this->urlGenerator->generate('home_page', ['_locale' => $preferredLanguage]));
-      $event->setResponse($response);
-    } else {
-      $response = new RedirectResponse($this->urlGenerator->generate('home_page', ['_locale' => $this->defaultLocale]));
-      $event->setResponse($response);
-    }
-  }
 }
